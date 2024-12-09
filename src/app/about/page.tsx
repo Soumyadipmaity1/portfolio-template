176fb1b7a23a0ba07255b58ab0b143a8ac2c2ee5
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-09 15:49:19 +0530

    Add About page enhancements: Update padding and margin for improved layout, and introduce a new profile image to enhance visual appeal and user engagement.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,16 +9,16 @@ export default function About() {
     <>
       <BackgroundAnimation />
       <Navigation isOpen={true} />
-      <div className="min-h-screen sm:pt-32 pt-24 px-6 pb-12">
+      <div className="min-h-screen sm:pt-32 pt-14 px-6 sm:pb-0">
         <div className="max-w-4xl mx-auto">
           <motion.div
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
           >
-            <h1 className="text-4xl md:text-5xl text-center sm:text-start font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-rose-500 mb-4">
+            <h1 className="text-4xl md:text-5xl text-center sm:text-start font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-rose-500 mb-2">
               About Me
             </h1>
-            <p className="text-gray-400 text-lg">
+            <p className="text-gray-400 my-2 mb-4 text-lg">
               Passionate about AI and Machine Learning
             </p>
           </motion.div>
